fddb0265a853f867f993ba8348cebb99ffe3ea8d
Emily <e_yeh1@u.pacific.edu>
2023-06-13 10:40:06 -0700

    Simple examples to test memory space in C programs

--- program_size_examples/executeAllPrograms.sh
+++ program_size_examples/executeAllPrograms.sh
@@ -5,10 +5,10 @@ echo > memorySizes.txt
 for i in `seq 1 5`;
 do
 	gcc 00$i*.c -o program$i
-	echo -e "\n" >> memorySizes.txt
 	echo 00$i*.c >> memorySizes.txt
 	size program$i >> memorySizes.txt
 	rm program$i
+	echo "" >> memorySizes.txt
 done
 
-echo -e "\nAll programs executed and output placed into txt file"
+echo All programs executed and output placed into memorySizes.txt
